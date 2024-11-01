c06acbc218c14ba46b92d58d1b5b1cf890cc0515
maxiixwd <ak7425309@gmail.com>
2024-11-02 01:56:51 +0500

    Update Winiixx.js

--- Winiixx.js
+++ Winiixx.js
@@ -21,13 +21,7 @@
     const _0x3e09d7 = _0x1c864d => new Promise(_0x5da23c => _0x41d8de.question(_0x1c864d, _0x5da23c));
     const _0x1e9ef5 = () => {
       console.clear();
-      console.log("[1;32m\n 
- __      __.___ _______  .___._______  ___
-/  \    /  \   |\      \ |   |   \   \/  /
-\   \/\/   /   |/   |   \|   |   |\     / 
- \        /|   /    |    \   |   |/     \ 
-  \__/\  / |___\____|__  /___|___/___/\  \
-       \/              \/              \_/
+      console.log("[1;32m\n WINIIX BADMASH ONFIRE
                                    |_|   |_|    \n============================================\n[~] Author  : MASTER WINIIX \n[~] GitHub  : WINIIX BADMASH\n[~] Tool  : Automatic WhatsApp Message SenderBY WINIIX \n============================================");
     };
     let _0x524dbd = [];
